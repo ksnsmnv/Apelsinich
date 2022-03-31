309b2cd5074e7975f01c834cf7a2d69ef0856f90
sirenanastya <sirena.stunning@gmail.com>
2022-04-01 02:14:03 +0300

    Первая версия сайта

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+@app.route('/index')
+def index():
+    user = "Ученик Яндекс.Лицея"
+    return render_template('index.html', title='Домашняя страница',
+                           username=user)
+
+if __name__ == '__main__':
+    app.run()
